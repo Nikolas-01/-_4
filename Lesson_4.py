d99356fd31864391a2867fb1c2bd73d637d3f1c1
ПК <89068414983@bk.ru>
2020-03-15 21:04:14 +0500

    Коммит2

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,10 +1,10 @@
 #1. Напишите функцию (F): на вход список имен и целое число N; на выходе список длины N случайных имен из первого списка
 # (могут повторяться, можно взять значения: количество имен 20, N = 100, рекомендуется использовать функцию random);
+print("Задача 1.")
 import random
-
 print()
 print("Lesson 1. Селект 100 имен из списка")
-list_names = 'Nik Gena Vlad Ivan Kolya Gera Natali Dunya Vilya Nikita Sergey Vitaliy Sarah Petr Vitya Seva Eleonora Max Volodya Roger'.split()
+list_names = 'Ник Гена Влад Иван Коля Гера Натали Дуня Виля Никита Сергей Виталий Сара Питр Витя Сева Элеонора Макс Володя Роджер'.split()
 def name_n(list_names, n):
     '''
     :param list_names: список имен
@@ -14,7 +14,6 @@ def name_n(list_names, n):
     list_names_random = []
     len_list = len(list_names)
     newName = ''
-
     for i in range(1, n + 1):
         # randomChoice - это случайное число в пределах длины начального списка,
         # случайным образом выбираем индекс
@@ -23,29 +22,41 @@ def name_n(list_names, n):
         newName = list_names[randomChoice]
         # Добавляем случайное слово newName в список
         list_names_random.append(newName)
-
     return list_names_random
-
 list_names_random = name_n(list_names, 100) # n == 100
 print('Список: \n%s\nКоличество слов: %s\nИзначально слов: %s' % (list_names_random, len(list_names_random), len(list_names)))
 print()
 print()
 ###############################################################################
-# 2. Напишите функцию вывода самого частого имени из списка на выходе функции F.
+# 2. Напишите функцию вывода самого частого имени из списка на выходе функции F;
 
-from collections import Counter
+print("Задача 2. \nСамое популярное имя:")
 
-def max_count(lst):
-    count = Counter(lst)
-    return max(count, key=lambda x: count[x])
+def popular_name(list_names_random):
+    # Создаем словарь, который считает количество упоминаний
+    list_names_random = {a: list_names_random.count(a) for a in list_names_random}
+    print(list_names_random) # для проверки - словарь с именем и количеством упоминаний
 
-print(Counter(list_F))
-print(*Counter(list_F).most_common(1))  # Можно было без max() в функции max_count(lst) обойтись оказывается ;)
-print(Counter(list_F).most_common(1)[0][0])
+    # Создаем список со значениями
+    values_list = []
 
-print(max_count(list_F))
+    for values in list_names_random.values():
+        values_list.append(values)
 
+    # из списка со значениями выбираем максимальное значение,
+    # создаем список из ключей, которым соответсвует максимальное значение
+    popular_names_list = []
+
+    for items, values in list_names_random.items():
+        if values == max(values_list):
+            popular_names_list.append(items)
+
+
+    return popular_names_list
+
+print('Имя:', popular_name(list_names_random))
 print()
+
 
 #########################################################################################################
 # 3. Напишите функцию вывода самой редкой буквы, с которого начинаются имена в списке на выходе функции F.

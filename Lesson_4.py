bf63137cad633cb8afc86f0b0ab88989013fe226
ПК <89068414983@bk.ru>
2020-03-15 19:18:50 +0500

    Коммит

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,24 +1,34 @@
-# 1. Напишите функцию (F):
-# на вход список имен и целое число N;
-# на выходе список длины N случайных имен из первого списка
-# (могут повторяться, можно взять значения: количество имен 20, N = 100,
-# рекомендуется использовать функцию random).
+#1. Напишите функцию (F): на вход список имен и целое число N; на выходе список длины N случайных имен из первого списка
+# (могут повторяться, можно взять значения: количество имен 20, N = 100, рекомендуется использовать функцию random);
+import random
 
-from random import choice
+print()
+print("Lesson 1. Селект 100 имен из списка")
+list_names = 'Nik Gena Vlad Ivan Kolya Gera Natali Dunya Vilya Nikita Sergey Vitaliy Sarah Petr Vitya Seva Eleonora Max Volodya Roger'.split()
+def name_n(list_names, n):
+    '''
+    :param list_names: список имен
+    :param n: длина списка на выходе
+    :return: список из 20 случайных имен
+    '''
+    list_names_random = []
+    len_list = len(list_names)
+    newName = ''
 
-def F(N, *args):
-    return [choice(args) for i in range(N)]
+    for i in range(1, n + 1):
+        # randomChoice - это случайное число в пределах длины начального списка,
+        # случайным образом выбираем индекс
+        randomChoice = random.randint(0, len_list-1)
+        # newName - это случайное слово по индексу
+        newName = list_names[randomChoice]
+        # Добавляем случайное слово newName в список
+        list_names_random.append(newName)
 
-print(F(3, 'aaaaaaa', 'bbbbbbbbb', 'ccccccccccc', 'dddddddd', 'eee', 'fffffffff'))
+    return list_names_random
 
-lst = ['0000000', '11111111', '22222222222', '3333333', '44444444',
-       '555555555', '666666666666', '777777777', '88888888888', '9999',
-       '000', '111111', '222222', '3333333', '444',
-       '555555555', '666666', '77777777', '88888', '9999999999']
-
-list_F = F(100, *lst)
-print(len(lst), len(list_F), list_F)
-
+list_names_random = name_n(list_names, 100) # n == 100
+print('Список: \n%s\nКоличество слов: %s\nИзначально слов: %s' % (list_names_random, len(list_names_random), len(list_names)))
+print()
 print()
 ###############################################################################
 # 2. Напишите функцию вывода самого частого имени из списка на выходе функции F.
